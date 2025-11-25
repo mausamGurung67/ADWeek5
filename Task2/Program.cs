315da2955fd20dddae864181160e9b4478baa91f
mausamGurung67 <mausamgurung678@gmail.com>
2025-11-25 11:23:27 +0545

    Completed task 2 successfully

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Car object
+            Car car = new Car();
+            car.Brand = "Toyota";
+            car.Speed = 120;
+            car.Seats = 5;
+
+            Console.WriteLine("Car Info");
+            car.Start();
+            car.DisplayInfo();
+            car.Stop();
+
+            Console.WriteLine();
+
+            // Motorcycle object
+            Motorcycle moto = new Motorcycle();
+            moto.Brand = "Yamaha";
+            moto.Speed = 90;
+            moto.HasCarrier = true;
+
+            Console.WriteLine("Motorcycle Info");
+            moto.Start();
+            moto.DisplayInfo();
+            moto.Stop();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Task2/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Vehicle
+    {
+        public string Brand { get; set; }
+        public int Speed { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine($"{Brand} is starting..");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"{Brand} is stopping..");
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h");
+        }
+    }
+}
